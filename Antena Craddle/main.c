4e85d6801cf75e932cf118fe6333135f9bbad749
Andres Barbudo Rodriguez <nerdcorestyle@gmail.com>
2021-10-04 20:54:06 -0300

    Initial Commit

---
+++ Antena Craddle/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Antena Craddle
+//
+//  Created by Andres Barbudo Rodriguez on 04-10-21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
